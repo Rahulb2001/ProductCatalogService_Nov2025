577afb10e563e81e329d78ba71b9fb4d81ac9a7c
khannaanurag1 <khannaanurag1@gmail.com>
2025-11-24 22:10:45 +0530

    initial commit

---
+++ src/main/java/org/example/productcatalogservice_nov2025evening/ProductCatalogServiceNov2025EveningApplication.java
@@ -0,0 +1,13 @@
+package org.example.productcatalogservice_nov2025evening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceNov2025EveningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogServiceNov2025EveningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/productcatalogservice_nov2025evening/ProductCatalogServiceNov2025EveningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productcatalogservice_nov2025evening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceNov2025EveningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
